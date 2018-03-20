02ba665297a607b2dcba828212a4b63f06f0ed2b
vluiten765348 <vluiten2907@gmail.com>
2018-03-20 13:35:53 +0100

    adding some comments

--- robotssrc/hybridattack/charger/Charger.java
+++ robotssrc/hybridattack/charger/Charger.java
@@ -3,29 +3,36 @@ package hybridattack.charger;
 import hybridattack.Generic.RobotReference;
 import robocode.HitRobotEvent;
 import hybridattack.Generic.HybridAttackBase;
+import robocode.ScannedRobotEvent;
+
+import java.util.ArrayList;
 
 public class Charger extends HybridAttackBase {
 
     @Override
     public void run() {
+        ArrayList<RobotReference> enemies;
+        RobotReference lowestHealth = null;
         while (true) {
             super.run();
-            if (getEnemy.size() > 1 && getTeam.size() > 1) {
-                RobotReference lowestHealth = null;
+            enemies = getEnemies();
+            if (enemies.size() > 1 && getTeam().size() > 1) {
                 for (RobotReference enemy : enemies) {
-                    if (lowestHealth == null || enemy.getEnergy() < lowestHealth.getEnergy() && enemy != teamTarget) {
-                        lowestHealth = enemy;
-                    }
-                    double relativeHeading = (lowestHealth.getLocation().subtract(location).getWorldBearing() - getHeading());
+                    if (lowestHealth == null || enemy.getEnergy() < lowestHealth.getEnergy()) {
+                        if (enemy != teamTarget) {
+                            lowestHealth = enemy;
+                        }
+                        if (!lowestHealth.isTeammate()) {
+                            turnLeft(lowestHealth.getLocation().subtract(location).getWorldBearing() + getHeading());
+                            ahead(500);
 
-                    if (relativeHeading < 0) {
-                        turnLeft(Math.abs(relativeHeading));
-                    } else {
-                        turnRight(Math.abs(relativeHeading));
+                    }
+
                     }
 
                 }
-                ahead(5000);
+
+
             }
 
         }
@@ -44,24 +51,28 @@ public class Charger extends HybridAttackBase {
     We want to kill the robot bij hitting him for additional bonus points.
     Bullet damage = 4 * firepower. If firepower > 1, it does an additional damage = 2 * (power - 1).
      */
+
     public void onHitRobot(HitRobotEvent e) {
-        if (e.getEnergy() > 16) {
-            fire(3);
-        } else if (e.getEnergy() > 13) {
-            fire(2.5);
-        } else if (e.getEnergy() > 10) {
-            fire(2);
-        } else if (e.getEnergy() > 7) {
-            fire(1.5);
-        } else if (e.getEnergy() > 4) {
-            fire(1);
-        } else if (e.getEnergy() > 2) {
-            fire(.5);
-        } else if (e.getEnergy() > .6) {
-            fire(.1);
+
+        if (!isTeammate(e.getName())) {
+
+            if (e.getEnergy() > 16) {
+                fire(3);
+            } else if (e.getEnergy() > 13) {
+                fire(2.5);
+            } else if (e.getEnergy() > 10) {
+                fire(2);
+            } else if (e.getEnergy() > 7) {
+                fire(1.5);
+            } else if (e.getEnergy() > 4) {
+                fire(1);
+            } else if (e.getEnergy() > 2) {
+                fire(.5);
+            } else if (e.getEnergy() > .6) {
+                fire(.1);
+            }
+            ahead(50);
         }
-        ahead(50);
+
     }
-
-
 }
