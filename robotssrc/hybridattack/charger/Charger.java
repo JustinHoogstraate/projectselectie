0347a6c0b857030464bd0ebed0ab536045323813
vluiten765348 <vluiten2907@gmail.com>
2018-03-20 11:36:21 +0100

    adding some comments

--- robotssrc/hybridattack/charger/Charger.java
+++ robotssrc/hybridattack/charger/Charger.java
@@ -3,5 +3,9 @@ package hybridattack.charger;
 import hybridattack.Generic.HybridAttackBase;
 
 public class Charger extends HybridAttackBase {
-
+    /* TODO
+    * Locking taget with low health
+    * locking target which is not a target for other enemies
+    *
+    * */
 }
