7d047f8019f99beceac86673965403b598888b01
vluiten765348 <vluiten2907@gmail.com>
2018-03-20 12:09:16 +0100

    adding some comments

--- robotssrc/hybridattack/charger/Charger.java
+++ robotssrc/hybridattack/charger/Charger.java
@@ -1,11 +1,46 @@
 package hybridattack.charger;
 
+import robocode.HitRobotEvent;
 import hybridattack.Generic.HybridAttackBase;
 
-public class Charger extends HybridAttackBase {
+    public class Charger extends HybridAttackBase {
     /* TODO
     * Locking taget with low health
     * locking target which is not a target for other enemies
     *
     * */
+
+
+
+    /*
+    On hit, Fire and hit him again
+    We want to kill the robot bij hitting him for additional bonus points.
+    Bullet damage = 4 * firepower. If firepower > 1, it does an additional damage = 2 * (power - 1).
+     */
+    public void onHitRobot(HitRobotEvent e){
+        if(e.getEnergy() > 16){
+            fire(3);
+        }
+        else if(e.getEnergy() > 13) {
+            fire(2.5);
+        }
+        else if(e.getEnergy() > 10) {
+            fire(2);
+        }
+        else if(e.getEnergy() > 7) {
+            fire(1.5);
+        }
+        else if(e.getEnergy() > 4){
+            fire(1);
+        }
+        else if (e.getEnergy() > 2 ){
+            fire(.5);
+        }
+        else if (e.getEnergy() > .6){
+            fire(.1);
+        }
+        ahead(50);
+    }
+
+
 }
